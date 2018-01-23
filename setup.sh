fd198263e4aa16de783d54ad6de551a7730b4896
Scott Varcoe <varcsscotty@gmail.com>
2018-01-22 16:14:10 -0800

    trying to surpress errors in script

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+set -e
+
 echo "deb https://packages.cloud.google.com/apt cloud-sdk-$(lsb_release -c -s) main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
@@ -14,19 +16,21 @@ DEBIAN_FRONTEND=noninteractive apt-get install -q -y 		\
 	python			\
 	python-pip		\
 	docker.io		\
-	unzip			
+	unzip			\
+	google-cloud-sdk
 
-apt-get install google-cloud-sdk -q -y
 apt-get clean -q -y
 
 
 wget https://github.com/ogham/exa/releases/download/v0.8.0/exa-linux-x86_64-0.8.0.zip
 unzip exa-linux-x86_64-0.8.0.zip
 mv exa-linux-x86_64 /usr/local/bin/exa
+chmod +x /usr/local/bin/exa
 export ls='exa -bhl --git'
 
 wget https://github.com/koki/short/releases/download/v0.4.0/short_linux_amd64 -q
 mv short_linux_amd64 /usr/local/bin/short
+chmod +x /usr/local/bin/short
 
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -q
 chmod +x ./install.sh
